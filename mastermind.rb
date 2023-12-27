8a2fed06edf7609b598e246ac2470c69a6241df1
Natalia <natalia.jadwiga.gajewska@gmail.com>
2023-12-27 14:07:43 +0100

    Easily to understand feedback

--- mastermind.rb
+++ mastermind.rb
@@ -4,7 +4,7 @@ CODE_LENGTH = 4
 NUMBER_RANGE = (0..9)
 
 # Generate secret code
-SECRET_CODE = [1, 2, 3, 4]
+SECRET_CODE = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].sample(4)
 
 # Provide Feedback Mechanism
 # '*' correct number in the correct position
@@ -14,13 +14,16 @@ SECRET_CODE = [1, 2, 3, 4]
 def provide_feedback(guess)
   feedback = []
   guess.each_with_index do |player_digit, index|
-    player_digit = player_digit.to_i
-        if SECRET_CODE[index] == player_digit
-          feedback << '*'
+      player_digit = player_digit.to_i
+    if SECRET_CODE[index] == player_digit
+  feedback << '*'
+          puts "You have chosen perfect place for #{player_digit}"
         elsif SECRET_CODE.include?(player_digit)
           feedback << '+'
+          puts "#{player_digit} includes in the Secret Code but it is in the wrong place!"
         else
           feedback << '-'
+          puts "#{player_digit } not includes in the Secret Code"
         end
   end
   feedback
@@ -38,7 +41,7 @@ def getting_player_guess
   loop do
   player_guess = gets.chomp.chars
 
-  until player_guess.length == CODE_LENGTH && player_guess.all? { |char| NUMBER_RANGE.cover?(char.to_i) } do
+  until player_guess.length == CODE_LENGTH 
     puts 'Invalid code. Please enter 4-digit code. Thank you!'
     player_guess = gets.chomp.chars
   end
