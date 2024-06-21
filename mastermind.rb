f6b2fa8aff39d8821725dd58d2526fcfb67995ea
Natalia <natalia.jadwiga.gajewska@gmail.com>
2024-06-21 23:16:59 +0200

    Refactored game code for readability and style compliance

--- mastermind.rb
+++ mastermind.rb
@@ -12,21 +12,19 @@ SECRET_CODE = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].sample(4)
 # '-' wrong number in the wrong position
  
 def provide_feedback(guess)
-  feedback = []
-  guess.each_with_index do |player_digit, index|
+  guess.map.with_index do |player_digit, index|
     player_digit = player_digit.to_i
     if SECRET_CODE[index] == player_digit
-      feedback << '*'
       puts "You have chosen perfect place for #{player_digit}"
+      '*'
     elsif SECRET_CODE.include?(player_digit)
-      feedback << '+'
       puts "#{player_digit} includes in the Secret Code but it is in the wrong place!"
+      '+'
     else
-      feedback << '-'
       puts "#{player_digit } not includes in the Secret Code"
+      '-'
     end
   end
-  feedback
 end
 
 def hello
@@ -38,22 +36,20 @@ end
 # Player's turn
 def getting_player_guess
   puts 'Please enter your 4 - digit guess code. :)'
-  loop do
+  player_guess = gets.chomp.chars
+
+  until player_guess.length == CODE_LENGTH
+    puts 'Invalid code. Please enter 4-digit code. Thank you!'
     player_guess = gets.chomp.chars
-
-    until player_guess.length == CODE_LENGTH
-      puts 'Invalid code. Please enter 4-digit code. Thank you!'
-      player_guess = gets.chomp.chars
-    end
-
-    puts "Your guess is: #{player_guess}"
-
-    return player_guess
   end
+
+  puts "Your guess is: #{player_guess.join}"
+
+  player_guess
 end
 
 def play_again?
-  puts "Do you want to play again?"
+  puts 'Do you want to play again?'
   answer = gets.chomp.downcase
   answer == 'yes'
 end
@@ -65,22 +61,30 @@ def game_loop
     player_guess = getting_player_guess
     feedback_result = provide_feedback(player_guess)
 
-    if feedback_result.all?('*')
-      puts 'Congrats! You have broke the code!'
-      break
-    end
+    break if code_broken?(feedback_result)
 
     turns_left -= 1
   end
 
+  display_end_message(turns_left)
+end
+
+def code_broken?(feedback_result)
+  if feedback_result.all?('*')
+    puts 'Congrats! You have broken the code!'
+    true
+  else
+    false
+  end
+end
+
+def display_end_message(turns_left)
   if turns_left.zero?
     puts "I'm sorry, you ran out of turns."
-    puts "Solution you were looking for was: #{SECRET_CODE}"
+    puts "Solution you were looking for was: #{SECRET_CODE.join}"
   end
 end
 
 hello
 game_loop
-while play_again?
-  game_loop
-end
+game_loop while play_again?
